0c464a45861dcca7fc42c5be16d3ed2a25a2a869
joseph <j.hernandez.92189@gmail.com>
2022-10-17 19:00:34 -0700

    added base file structure for server and database creations

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.MYSQL_DB,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
+
+module.exports = sequelize;
