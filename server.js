88e3affdd4ad335aaacaf4c7c341cddf77b52335
joseph <j.hernandez.92189@gmail.com>
2022-10-17 19:02:44 -0700

    saved server.js

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
-// Gives us access to fs module methods
-const fs = require('fs');
-// Inquirer
-const inquirer = require('inquirer');
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
+
+const sequelize = require('./config/connection');
+
+const routes = require('./controller');
+
+// confiiguration for handlebars
+const hbs = exphbs.create({});
+
+// 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sessionSetting = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+};
+
+//we setup handlebars and connect it with express
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sessionSetting));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+});
