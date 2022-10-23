332704a917e66a8dbbbf4341cc3e9d25b000e763
“Gigi <debortolg@gmail.com>
2022-10-23 11:40:30 -0700

    adding template for logging out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// CREATE new user
+// CREATE new user // login 
 router.post('/', async (req, res) => {
     try {
       const dbUserData = await User.create({
@@ -23,8 +23,17 @@ router.post('/', async (req, res) => {
     }
   });
 
-// Login
 
 // Logout
+
+// router.post('/logout', (req, res) => {
+//   if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
 module.exports = router; 
