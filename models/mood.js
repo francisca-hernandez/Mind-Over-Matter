22434242b486e55980cae9fb13e316d9ec198433
francisca hernandez <francisca.hernandez17@gmail.com>
2022-10-23 22:38:40 -0700

    updated moodjs file, still needing help

--- models/mood.js
+++ models/mood.js
@@ -2,4 +2,28 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const sequelize = require('../config/connection');
 
-// this is calling the array from userMoodAR to disply to the html to user 
+Mood.init (
+
+    text: {
+
+
+        type: DataTypes.STRING,
+        allowNull: false,
+
+
+    },
+    
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'mood'
+    }
+
+
+
+);
+
+
+    module.exports = Mood;
