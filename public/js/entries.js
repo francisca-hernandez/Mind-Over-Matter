a40f3112dd991690591f506889adb17dccd44f82
“Gigi <debortolg@gmail.com>
2022-10-25 23:34:13 -0700

    added moods and date to the page as cards

--- public/js/entries.js
+++ public/js/entries.js
@@ -1,7 +1,25 @@
-fetch("/api/users/mood").then(function(response) {
+let $moodContainer = document.getElementById('mood-container');
+
+fetch("/api/users/mood")
+.then(function(response) {
     if(response.ok) {
         response.json().then(function(data) {
             console.log(data);
-        })
-    }
+
+            for (i = 0; i < data.length; i++) {
+            let $moodCard = document.createElement("div");
+                console.log(data[i].mood);
+
+            let $moodTitle = document.createElement("h1");
+            $moodTitle.textContent = data[i].mood
+
+            let $moodDate = document.createElement("p");
+            $moodDate.textContent = data[i].createdAt;
+
+            $moodCard.appendChild($moodTitle);
+            $moodCard.appendChild($moodDate);
+            $moodContainer.appendChild($moodCard);
+            
+        }
+    })}
 });
