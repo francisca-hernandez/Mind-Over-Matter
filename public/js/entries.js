9ac50a851feac86e4e7e18d57d01f418e9535056
davidricksgers <davidricksgers@gmail.com>
2022-10-26 01:19:32 -0700

    mood display moved to entries

--- public/js/entries.js
+++ public/js/entries.js
@@ -1,26 +1,31 @@
 let $moodContainer = document.getElementById('mood-container');
 
 fetch("/api/users/mood")
-.then(function(response) {
-    if(response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
 
-            for (i = 0; i < data.length; i++) {
-            let $moodCard = document.createElement("article");
-            $moodCard.classList = ("uk-section uk-section-small uk-padding-remove-top")
-                console.log(data[i].mood);
+                for (i = 0; i < data.length; i++) {
+                    let $moodhead = document.createElement("h3")
+                    $moodhead.classList = ("uk-heading-line uk-text-bold")
+                    let $moodCard = document.createElement("article");
+                    $moodCard.classList = ("uk-section uk-section-small uk-padding-remove-top")
+                    console.log(data[i].mood);
 
-            let $moodTitle = document.createElement("h2");
-            $moodTitle.textContent = data[i].mood
+                    let $moodTitle = document.createElement("h2");
+                    $moodTitle.textContent = data[i].mood
 
-            let $moodDate = document.createElement("h3");
-            $moodDate.textContent = data[i].createdAt;
+                    let $moodDate = document.createElement("Span");
+                    $moodDate.textContent = data[i].createdAt;
 
-            $moodCard.appendChild($moodTitle);
-            $moodCard.appendChild($moodDate);
-            $moodContainer.appendChild($moodCard);
-            
+                    $moodCard.appendChild($moodTitle);
+                    $moodhead.appendChild($moodDate);
+                    $moodContainer.appendChild($moodhead)
+                    $moodContainer.appendChild($moodCard);
+
+
+                }
+            })
         }
-    })}
-});
+    });
