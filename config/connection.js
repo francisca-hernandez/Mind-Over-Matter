96dbbefc1a0668ea47f5893702a0d23fe0ffbfbd
davidricksgers <davidricksgers@gmail.com>
2022-10-26 18:40:31 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,19 @@ const Sequelize = require('sequelize');
 let sequelize;
 
 
-// if (process.env.JAWSDB_URL) {
-//     sequelize = new Sequelize(process.env.JAWSDB_URL)
-// } else {
-//     sequelize = new Sequelize(
-//         process.env.MYSQL_DB,
-//         process.env.MYSQL_USER,
-//         process.env.MYSQL_PASSWORD,
-//         {
-//             host: 'localhost',
-//             dialect: 'mysql',
-//             port: 3306,
-//         }
-//     );
-// }
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.MYSQL_DB,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
 
 module.exports = sequelize;
